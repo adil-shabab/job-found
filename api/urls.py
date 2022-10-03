4bb9905afc1a94b602ff017c821990382b9342a0
Adil Shabab <101416092+adil-shabab@users.noreply.github.com>
2022-10-03 14:29:59 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 from .views import *
 
-
-from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
