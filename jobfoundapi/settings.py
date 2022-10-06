e5b14941c0cdfa4ecb8e28b9817e9d88e093c799
Adil Shabab <adilshabab2004@gmail.com>
2022-10-06 16:00:37 +0530

    some more changes added

--- jobfoundapi/settings.py
+++ jobfoundapi/settings.py
@@ -154,6 +154,10 @@ STATICFILES_DIRS=[
     
 
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
--- jobfoundapi/urls.py
+++ jobfoundapi/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
 ]
-if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
